c9da3d78d6b6debcb7e75d79c316656b71afa371
Your Name <you@example.com>
2020-05-27 03:53:07 +0200

    Initial Commit

---
+++ StanfordSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StanfordSwiftUI
+//
+//  Created by Ahmed-Ali on 5/27/20.
+//  Copyright © 2020 com.spik.swiftui. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
